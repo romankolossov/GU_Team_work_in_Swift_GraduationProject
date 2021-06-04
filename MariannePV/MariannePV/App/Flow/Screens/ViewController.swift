0ac224f9e1b8749e329175dfe1fcbdc84f7dfb4f
romankolossov <rkolossov@gmail.com>
2021-06-04 20:37:24 +0300

    MariannePV file structure organization done

---
+++ MariannePV/MariannePV/App/Flow/Screens/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MariannePV
+//
+//  Created by Roman Kolosov on 01.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+}
